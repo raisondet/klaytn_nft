0d79b9a6c5f626b954d8dcf9d5c7fbde06d4ff07
cch <raisondet@gmail.com>
2022-02-19 20:35:36 +0900

    add ABI and address file

--- nftContract/contracts/Who1NFT.sol
+++ nftContract/contracts/Who1NFT.sol
@@ -9,18 +9,43 @@ contract Who1NFT is KIP17Full, Ownable {
   using Counters for Counters.Counter;
   Counters.Counter private tokenId;
 
-  string nftName = "WHO1";
-  string nftSymbol = "WHN";
+  string public nftName = "WHO1";
+  string public nftSymbol = "WHN";
+  bool private locked = false;
+
   constructor() public KIP17Full(nftName, nftSymbol){
   }
 
-  function mintNFT(string memory _tokenURI) public onlyOwner returns (uint256) {
-    tokenId.increment();
+  modifier validAddress(address _addr){
+    require(_addr != address(0), "Not valid address");
+    _;
+  }
 
+  modifier noReentrancy() {
+    require(!locked, "No reentrancy");
+    locked = true;
+    _;
+    locked = false;
+  }
+
+  function mintNFT(string memory _tokenURI) public onlyOwner noReentrancy returns (uint256) {
+    tokenId.increment();
     uint256 newId = tokenId.current();   
     _mint(msg.sender, newId);
     _setTokenURI(newId, _tokenURI);
 
     return newId; 
+  }
+
+  function transferNFT(address _to, uint256 _tokenId) public onlyOwner {
+    safeTransferFrom(msg.sender, _to, _tokenId);
+  }
+
+  function burnNFT(uint256 _tokenId) public {
+    _burn(_tokenId);
+  }
+
+  function getTotalCount() public view returns (uint256) {
+    return tokenId.current();
   }
 }
